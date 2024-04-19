5eb56b7baabe7a43fe26b5156895a7fb1e56db43
heesangh <heesang@vt.edu>
2024-04-19 19:04:53 -0400

    heesang - add a new page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.sass";
 import ExamplePage1 from "./Pages/ExamplePage1/ExamplePage1.js";
 import ExamplePage2 from "./Pages/ExamplePage2/ExamplePage2.js";
+import Heesang from "./Pages/Heesang/Heesang.js";
 
 function App() {
   return (
     <div className="App">
       <ExamplePage1 />
       <ExamplePage2 />
+      <Heesang />
     </div>
   );
 }
