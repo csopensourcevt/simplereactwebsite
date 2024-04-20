4da269df64f1329d29763d76617e3ed64ed81b82
csopensourcevt <jnnglrlol@gmail.com>
2024-04-19 23:11:28 -0400

    added forest image

--- src/Pages/Heesang/Heesang.js
+++ src/Pages/Heesang/Heesang.js
@@ -1,9 +1,12 @@
+import "./Heesang.sass";
+import Forest from "../../Components/Images/forest.jpg";
+
 function Heesang() {
-    return (
-        <div className="heesang_toplevel">
-            Heesang
-        </div>
-    );
+  return (
+    <div className="heesang_toplevel">
+      <img src={Forest} className="heesang_image" />
+    </div>
+  );
 }
 
 export default Heesang;
