b37f96cf2bd34884c365ab0f4d66c55925d74df1
lsalinas1203 <lsalinas1203@vt.edu>
2024-04-20 17:58:15 -0400

    Add my information

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.sass";
 import ExamplePage1 from "./Pages/ExamplePage1/ExamplePage1.js";
 import ExamplePage2 from "./Pages/ExamplePage2/ExamplePage2.js";
 import Heesang from "./Pages/Heesang/Heesang.js";
+import Salinas from "./Pages/Salinas/Salinas.js";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <ExamplePage1 />
       <ExamplePage2 />
       <Heesang />
+      <Salinas />
     </div>
   );
 }
